d7642e5682763f6307e13da2d3f8140cb57b307e
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2022-11-23 12:10:03 +0200

    fixed component header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,18 @@ const Header = () => {
 
     return (
         <header className="header">
-            <NavLink className={({isActive}) => (isActive ? "active" : "") || 'home'} to='/'>
+            <NavLink className={({isActive}) => (isActive ? "active" : "")} to='/'>
                 HOME
             </NavLink>
             <nav className="nav">
                 <ul>
                     <li>
-                        <NavLink to='users' className={({isActive}) => (isActive ? "active" : "") || 'users'}>
+                        <NavLink to='users' className={({isActive}) => (isActive ? "active" : "")}>
                             USERS
                         </NavLink>
                     </li>
                     <li className="li">
-                        <NavLink to='hotels' className={({isActive}) => (isActive ? "active" : "") || 'users'}>
+                        <NavLink to='hotels' className={({isActive}) => (isActive ? "active" : "")}>
                             HOTELS
                         </NavLink>
                     </li>
